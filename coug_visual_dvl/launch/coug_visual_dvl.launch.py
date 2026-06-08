c83ded3e47fb5ef3ef8a2123ec9f866e14244dc5
Nelson Durrant <snelsondurrant@gmail.com>
2026-06-08 12:54:32 -0600

    Add BAGS_DIR

--- coug_visual_dvl/launch/coug_visual_dvl.launch.py
+++ coug_visual_dvl/launch/coug_visual_dvl.launch.py
@@ -30,14 +30,14 @@ def generate_launch_description() -> LaunchDescription:
 
     fleet_params = PathJoinSubstitution(
         [
-            EnvironmentVariable("CONFIG_FOLDER"),
+            EnvironmentVariable("CONFIG_DIR"),
             "fleet",
             "coug_visual_dvl_params.yaml",
         ]
     )
     auv_params = PathJoinSubstitution(
         [
-            EnvironmentVariable("CONFIG_FOLDER"),
+            EnvironmentVariable("CONFIG_DIR"),
             PythonExpression(["'", auv_ns, "' + '_params.yaml'"]),
         ]
     )
