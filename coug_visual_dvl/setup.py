71b01525b8b436cec3b34e90b00f4ff6e775ad4d
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-01 10:46:28 -0600

    Remove native GUI configs

--- coug_visual_dvl/setup.py
+++ coug_visual_dvl/setup.py
@@ -15,7 +15,7 @@ setup(
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
         (
             os.path.join("share", package_name, "config"),
-            glob("config/*.yaml") + glob("config/*.xml.template"),
+            glob("config/*.yaml"),
         ),
     ],
     install_requires=["setuptools"],
